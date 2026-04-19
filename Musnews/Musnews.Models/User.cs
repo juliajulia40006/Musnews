ebc9ca763b58b6b44eac6219053eb8915b2a4908
Zaryamiya <julia.julia40006@gmail.com>
2026-04-19 13:14:27 +0300

    added user.cs

---
+++ Musnews/Musnews.Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musnews.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+    }
+}
