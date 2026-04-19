0eb62572234644c9efc94047ebfe4e2791ff90fc
Zaryamiya <julia.julia40006@gmail.com>
2026-04-19 13:18:04 +0300

    added TrackDto.cs

---
+++ Musnews/Musnews.Models/DTO/TrackDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musnews.Models.DTO
+{
+    internal class TrackDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Artist { get; set; } = string.Empty;
+    }
+}
