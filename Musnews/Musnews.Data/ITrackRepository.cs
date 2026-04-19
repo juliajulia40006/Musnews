b1096e770f52aeb7d3805e6524571afdfa85eaa3
Zaryamiya <julia.julia40006@gmail.com>
2026-04-19 13:23:20 +0300

    added ITrackRepository

---
+++ Musnews/Musnews.Data/ITrackRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Musnews.Models;
+
+namespace Musnews.Data
+{
+    public interface ITrackRepository
+    {
+        Task<IEnumerable<Track>> GetAllAsync();
+        Task<Track?> GetByIdAsync(int id);
+        Task AddAsync(Track track);
+    }
+}
