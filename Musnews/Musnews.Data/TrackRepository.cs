1ea29b053456f88000990d48998fbea55aa0450c
Zaryamiya <julia.julia40006@gmail.com>
2026-04-19 13:24:47 +0300

    added TrackRepository.cs

---
+++ Musnews/Musnews.Data/TrackRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Musnews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musnews.Data
+{
+    public class TrackRepository : ITrackRepository
+    {
+        private readonly AppDbContext _context;
+        public TrackRepository(AppDbContext context) => _context = context;
+
+        public async Task<IEnumerable<Track>> GetAllAsync()
+            => await _context.Tracks.ToListAsync();
+
+        public async Task<Track?> GetByIdAsync(int id)
+            => await _context.Tracks.FindAsync(id);
+
+        public async Task AddAsync(Track track)
+        {
+            await _context.Tracks.AddAsync(track);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
