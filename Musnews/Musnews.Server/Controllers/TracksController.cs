deb81f1ec293bf33b99a8ffd653d3f94ab257e20
Zaryamiya <julia.julia40006@gmail.com>
2026-04-19 13:32:49 +0300

    added TrackControllers.cs

---
+++ Musnews/Musnews.Server/Controllers/TracksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Musnews.Data;
+using Musnews.Models;
+
+namespace Musnews.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TracksController : ControllerBase
+{
+    private readonly ITrackRepository _trackRepo;
+
+    public TracksController(ITrackRepository trackRepo)
+    {
+        _trackRepo = trackRepo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var tracks = await _trackRepo.GetAllAsync();
+        return Ok(tracks);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var track = await _trackRepo.GetByIdAsync(id);
+        return track is null ? NotFound() : Ok(track);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Track track)
+    {
+        await _trackRepo.AddAsync(track);
+        return CreatedAtAction(nameof(GetById), new { id = track.Id }, track);
+    }
+}
