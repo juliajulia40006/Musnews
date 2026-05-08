7dc4479d4eed4d18ebe855acee2611bf0e0416ff
Zaryamiya <julia.julia40006@gmail.com>
2026-05-08 15:12:33 +0300

    update user.cs

--- Musnews/Musnews.Models/User.cs
+++ Musnews/Musnews.Models/User.cs
@@ -10,5 +10,7 @@ namespace Musnews.Models
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
